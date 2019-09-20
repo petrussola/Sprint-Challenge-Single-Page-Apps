0dfd248c91d8e9f48b52be7edd01a446a80f5d25
Pere <pere.solaclaver@gmail.com>
2019-09-20 09:35:14 +0100

    Style header and nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+  .nav-menu {
+    width: 33%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    a {
+      text-decoration: none;
+      color: inherit;
+      &:hover {
+      border-bottom: 1px dashed #CCCCCC;
+      }
+    }
+  }
+`;
 
 export default function Header() {
   return (
-    <header className="ui centered">
+    <StyledHeader className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/characters'>Characters</NavLink>
-    </header>
+      <div className='nav-menu'>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/characters">Characters</NavLink>
+      </div>
+    </StyledHeader>
   );
 }
